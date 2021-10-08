97983e4a2a6801e52b46d34e131338096c1649ff
petekofod <freeat12five@gmail.com>
2021-10-08 16:30:59 -0400

    Skeleton Test that fails

---
+++ src/src/test/java/com/collins/railwaynet/strolrloglambda/LogFileIndexerTest.java
@@ -0,0 +1,20 @@
+package com.collins.railwaynet.strolrloglambda;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LogFileIndexerTest {
+	
+	@Before
+	public void setup() {
+		System.out.println("Setup");
+	}
+
+	@Test
+	public void testHandleRequest() {
+		fail("Not yet implemented");
+	}
+
+}
